565cec1a74ba993f382348b9d2c1fa1fc01b438e
ganeshsundaram39 <ganeshsundaram39@gmail.com>
2020-02-01 22:54:32 +0530

    remove google login

--- issue-tracker-frontend/src/app/authentication/login/login.component.ts
+++ issue-tracker-frontend/src/app/authentication/login/login.component.ts
@@ -3,7 +3,7 @@ import { Router, ActivatedRoute } from '@angular/router';
 import { AuthenticationService } from '../authentication.service';
 import { FormGroup, Validators, FormControl } from '@angular/forms';
 import { MatSnackBar } from '@angular/material/snack-bar';
-declare const gapi: any;
+
 
 @Component({
   selector: 'app-login',
@@ -22,11 +22,6 @@ export class LoginComponent implements OnInit {
     private authService: AuthenticationService,
     private _snackBar: MatSnackBar
   ) {
-    if (gapi) {
-      gapi.load('auth2', function () {
-        gapi.auth2.init();
-      });
-    }
   }
 
   ngOnInit() {
@@ -40,25 +35,15 @@ export class LoginComponent implements OnInit {
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
         Validators.required,
-        Validators.pattern('/^[A-Za-z0-9]w{7,}$/')
+        Validators.minLength(8),
+        Validators.pattern('^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$')
       ])
     });
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       password: new FormControl('', [
         Validators.required,
-        Validators.pattern('/^[A-Za-z0-9]w{7,}$/')
       ])
-    });
-  }
-
-
-  googleLogin() {
-    const googleAuth = gapi.auth2.getAuthInstance();
-    googleAuth.then(() => {
-      googleAuth.signIn({ scope: 'profile email' }).then(googleUser => {
-        console.log(googleUser.getBasicProfile());
-      });
     });
   }
 
@@ -69,51 +54,55 @@ export class LoginComponent implements OnInit {
     }
     this.router.navigate(['../register'], { relativeTo: this.route });
   }
-  getEmailErrorMessage() {
-    return this.loginForm.get('email').errors.required
-      ? 'Email is required'
-      : this.loginForm.get('email').errors.email
-        ? 'Not a valid email'
-        : '';
-  }
-  getEmailSignupErrorMessage() {
-    return this.signupForm.get('email').errors.required
-      ? 'Email is required'
-      : this.signupForm.get('email').errors.email
-        ? 'Not a valid email'
-        : '';
-  }
-  getPasswordErrorMessage() {
-    return this.signupForm.get('password').errors.required
-      ? 'Password is required'
-      : this.signupForm.get('password').errors.pattern
-        ? 'Not a valid password'
-        : '';
-  }
+
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
       duration: 2000,
     });
   }
+
   onRegister() {
     this.authService.signup(this.signupForm.value).subscribe(
-      response => {
+      (response: any) => {
         console.log(response);
-        this.openSnackBar('Registered..!!', 'Success');
-        this.router.navigate(['/auth/login']);
+        if (response.error) {
+          this.openSnackBar(response.message, 'Error');
+        } else {
+          this.openSnackBar('Registered..!!', 'Success');
+          this.router.navigate(['/auth/login']);
+        }
       },
       error => {
-        console.log(error);
+        if (error && error.error) {
+          this.openSnackBar(error.error.message, 'Error');
+        } else {
+          console.log(error);
+          this.openSnackBar('Something went wrong..!!', 'Error');
+        }
       }
     );
   }
+
   onLogin() {
     this.authService.login(this.loginForm.value).subscribe(
-      response => {
+      (response: any) => {
         console.log(response);
+        if (response.error) {
+          this.openSnackBar(response.message, 'Error');
+        } else {
+          if (response.message === 'Login Successful' && response.data) {
+            localStorage.setItem('userdata', JSON.stringify({ ...response.data }));
+            this.router.navigate(['/issues']);
+          }
+        }
       },
       error => {
         console.log(error);
+        if (error && error.error) {
+          this.openSnackBar(error.error.message, 'Error');
+        } else {
+          this.openSnackBar('Something went wrong..!!', 'Error');
+        }
       }
     );
   }
